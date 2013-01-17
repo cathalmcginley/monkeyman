935540ea27563bb83bce52429bd419894655d20b
Cathal Mc Ginley <cathal.magus@gnostai.org>
2013-01-17 00:53:04 +0000

    Relicensed this fork under the GNU GPL version 3 or later.
    Mostly, this is to make the program license-compatible with its many dependencies,
    most of which are Apache 2.0 licensed (incompatible with GPL v2, compatible with v3).

--- src/main/scala/nl/flotsam/monkeyman/MonkeymanOptions.scala
+++ src/main/scala/nl/flotsam/monkeyman/MonkeymanOptions.scala
@@ -1,22 +1,24 @@
 /*
- * Monkeyman static web site generator
- * Copyright (C) 2012  Wilfred Springer
- * Copyright (C) 2013  Cathal Mc Ginley
+ * This file is part of Monkeyman, a static web site generator.
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * Copyright (C) 2012 Wilfred Springer
+ * Copyright (C) 2013 Cathal Mc Ginley
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * Monkeyman is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * Monkeyman is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * along with Monkeyman; see the file COPYING. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
+
 package nl.flotsam.monkeyman
 
 class MonkeymanOption(val name: String, val on: Boolean) {
